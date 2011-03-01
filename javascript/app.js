a39442330842f63b3951c778cc369ca452580dfd
james <james@b2545d00-38c7-4a61-b55b-be9ae9d93c16>
2011-03-01 04:38:18 +0000

    saving progress on drag and drop
    git-svn-id: http://jamesstarmer.com/svn/netswork/trunk@15 b2545d00-38c7-4a61-b55b-be9ae9d93c16

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,10 @@
 dojo.addOnLoad(
   function(){
 	
-	NW.objects = []
+	NW.objects = [];
 	
 	$('#btn-line').bind('click', function(e){
-		var newJoint = Joint({x: 50, y: 100}, {x: 100, y: 50}, NW.line).register(all);
+		var newJoint = Joint({x: 50, y: 100}, {x: 100, y: 50}, NW.line).register(NW.objects);
 		Joint.dia.registerJoint(newJoint);
 		e.preventDefault();
 	});
@@ -21,46 +21,45 @@ dojo.addOnLoad(
 		stop: function(event, ui) {
 			var newX = ui.position.left - $('#diagram').offset().left;
 			var newY = ui.position.top - $('#diagram').offset().top + 30;
+			
 			var cloud = network.cloud.create({
 			  position: {x: newX, y:newY},
-			  label: "object 3"
+			  label: "Internet"
 			});
+			
+			NW.objects.push(cloud);
+			
+			var joints = Joint.dia.registeredJoints();
+			for(var i = 0; i < joints.length; i++){
+				var joint = joints[i];
+				joint.register(NW.objects);
+			}
 		}
 	});
 	
-	
-	
 	var network = Joint.dia.network;
 	Joint.paper("diagram", '100%', 600);
-	console.log(Joint.dia.registeredJoints().length);
 
 	var s1 = network.cloud.create({
 	  position: {x: 120, y: 70},
-	  label: "object 1"
+	  label: "Internet"
 	});
-	
-	console.log(Joint.dia.registeredJoints().length);
 	
 	var s2 = network.cloud.create({
 	  position: {x: 250, y: 100},
-	  label: "object 2"
+	  label: "Internet"
 	});
-	
-	console.log(Joint.dia.registeredJoints().length);
 	
 	var s3 = network.cloud.create({
 	  position: {x: 275, y: 175},
-	  label: "object 3"
+	  label: "Internet"
 	});
 
-	console.log(Joint.dia.registeredJoints().length);
+	NW.objects.push(s1, s2, s3);
 
-	var all = [s1, s2, s3];
-
-	s1.joint(s2, NW.line).register(all);
+	s1.joint(s2, NW.line).register(NW.objects);
 	
-	s3.joint({x:300,y:300}, NW.line).register(all);
+	s3.joint({x:300,y:300}, NW.line).register(NW.objects);
 
-	console.log(Joint.dia.registeredJoints().length);
 
 });
